fea36af9f85c34d5db85ac25c8928d43a025d7a5
Cristal Zavala <czavala7133@southwestuniversity.edu>
2021-05-25 11:51:17 -0600

    this my six assignment

--- HomeworkCis131/HomeworkCis131/Program.cs
+++ HomeworkCis131/HomeworkCis131/Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using HomeworkCis131.Models;
 
 namespace HomeworkCis131
 {
-    class Program
-    
+
+    public class Program
+    {
         static void Main(string[] args)
-
         {
+            int[] myArray = new int[10];
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
+            myArray[9] = 10;
 
-            int result = 0;
+            for (var i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+            List<string> myList = new List<string>();
 
 
-            for (var i=1;1<= 20;i++) ;
+            myList.Add("1");
+            myList.Add("2");
+            myList.Add("3");
+            myList.Add("4");
+            myList.Add("5");
+            myList.Add("6");
+            myList.Add("7");
+            myList.Add("8");
+            myList.Add("9");
+            myList.Add("10");
+
+
+            foreach (var item in myList)
+            {
+
+                Console.WriteLine(item);
+            }
         }
 
-        {
-        result = result + i;
-        }
-Console.WriteLine(result);
+    }
         
      
 
