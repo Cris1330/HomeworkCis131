2dd85ce68b158e00317c462d3a2f3a7ad7d6b449
Cristal Zavala <czavala7133@southwestuniversity.edu>
2021-05-25 08:14:59 -0600

    this is module 1

---
+++ HomeworkCis131/HomeworkCis131/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeworkCis131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Class!");
+
+            int a = 45;
+            int b = 35;
+            int c = 0;
+
+            c = a * b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
